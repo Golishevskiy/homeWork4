c3aaf9e59f264ce98b69a47aaeb3eb62912e287f
Peter <golishevskiypetro@ukr.net>
2019-02-12 13:03:33 +0200

    task 8

--- homeWork4/ViewController.swift
+++ homeWork4/ViewController.swift
@@ -12,19 +12,22 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        countOfCharacter()
-//        catchSuffix()
-//        checkFullName()
-//        print(reversString(text: "Hello"))
-        
+        countOfCharacter()
+        catchSuffix()
+        checkFullName()
+        print(reversString(text: "Hello"))
         print(commaInNumber(number: 2949477789))
+        print(sortArray(array: [10,7,11,11, 1,2,3,4,5]))
+        print(translite(str: "abcfD"))
     }
     
+    //homework 1
     func countOfCharacter() {
         let name = "Petro"
         print("Count characters in name = \(name.count)")
     }
     
+    //homework 2
     func catchSuffix() {
         let lastName = "Голішевський"
         if lastName.hasSuffix("ий") {
@@ -32,15 +35,7 @@ class ViewController: UIViewController {
         }
     }
     
-    //    func checkFullName() {
-    //        var firstName = ""
-    //        var lostname = ""
-    //        var fullName = "GolishevskiyPetro"
-    //
-    //
-    //        print(fullName.uppercased())
-    //    }
-    
+    //homework 4 revers the string
     func reversString(text: String) -> String {
         var reversText = ""
         for i in text {
@@ -48,7 +43,8 @@ class ViewController: UIViewController {
         }
         return reversText
     }
-    // comma in number each 3 symbol
+    
+    // homework 5 comma in number each 3 symbol
     func commaInNumber(number: Int) -> String {
         let numberInString = String(number)
         var charArray = [Character]()
@@ -64,6 +60,7 @@ class ViewController: UIViewController {
         let stringDone = charToString(array: noComma)
         return stringDone
     }
+    
     // if first char is comma - remove comma and return array
     func checkFirstCharComma(array: [Character]) -> [Character] {
         var checkArray = array
@@ -72,6 +69,7 @@ class ViewController: UIViewController {
         }
         return checkArray
     }
+    
     // conver [char] to String
     func charToString(array: [Character]) -> String {
         var inString = ""
@@ -81,11 +79,53 @@ class ViewController: UIViewController {
         return inString
     }
     
-//    func commaInNumber(number: Int) -> String {
-//        var numberString = String(number)
-//
-//        return " "
-//    }
+    // homework 7
+    func sortArray(array: [Int]) -> [Int] {
+        var arrayForSorted = array
+        var resultArray = [Int].init()
+        while !arrayForSorted.isEmpty {
+            let minValue = arrayForSorted.min()!
+            if !resultArray.contains(minValue) {
+                resultArray.append(minValue)
+            }
+            arrayForSorted.remove(at: arrayForSorted.index(of: minValue)!)
+        }
+        return resultArray
+    }
     
+    //homework 8
+    func translite(str: String) -> String {
+        let alphabet = ["a": "a", "b": "б", "c": "ц", "d": "д"]
+        var transliteString = String.init()
+        for i in str {
+            if String(i) == String(i).lowercased() {
+                transliteString.append(contentsOf: alphabet[String(i)] ?? "")
+            } else {
+                let lowString = String(i).lowercased()
+                transliteString.append(contentsOf: alphabet[lowString]?.uppercased() ?? "")
+            }
+        }
+        return transliteString
+    }
+    
+    //homework 10
+    func antimat(str: String) -> String {
+        
+    }
+    
+    
+    //put a space between the firstname and secondname
+    func checkFullName() {
+//        var firstName = ""
+//        var lostname = ""
+        let fullName = "GolishevskiyPetro"
+        for i in fullName {
+            var stringSymbol = String(i)
+            if stringSymbol.uppercased() == stringSymbol {
+                print(stringSymbol)
+            }
+        }
+        print(fullName)
+    }
 }
 
