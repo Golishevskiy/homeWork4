4ed32d690de1d304dbfb627b7c4769f27f22fc94
Peter <golishevskiypetro@ukr.net>
2019-02-14 16:33:28 +0200

    task 1-9

--- homeWork4/ViewController.swift
+++ homeWork4/ViewController.swift
@@ -17,10 +17,11 @@ class ViewController: UIViewController {
 //        print(reversString(text: "Hello"))
 //        print(commaInNumber(number: 2949477789))
 //        print(sortArray(array: [10,7,11,11, 1,2,3,4,5]))
-//        print(translite(str: "abcfD"))
+//        let array = ["lada", "sedan", "baklazhan", "chemodan"]
+//        checkWords(words: array, searchWords: "da")
+//        print(toDivideName(fullname: "GolishevskiyPetro"))
+        passwordStrength(str: "Hs@#dgjk1")
         
-        let array = ["lada", "sedan", "baklazhan", "chemodan"]
-        checkWords(words: array, searchWords: "da")
     }
     
     //homework 1
@@ -35,6 +36,22 @@ class ViewController: UIViewController {
         if lastName.hasSuffix("ий") {
             print("lastName \(lastName) has \"ий\"")
         }
+    }
+    
+    //homework 3
+    func toDivideName(fullname: String) -> [String] {
+        var nameArray = [String]()
+        var iteration = -1
+        for value in fullname {
+            if String(value) == String(value).uppercased() {
+                nameArray.append(String(value))
+                iteration += 1
+            }
+            else {
+                nameArray[iteration].append(String(value))
+            }
+        }
+        return nameArray
     }
     
     //homework 4 revers the string
@@ -81,6 +98,55 @@ class ViewController: UIViewController {
         return inString
     }
     
+    //homework 6
+    func passwordStrength(str: String) {
+        var level = 0  //can have 4 levels of reliability
+        level += checkToNumber(password: str)
+        level += checkToUpperCase(password: str)
+        level += checkToSpecialChar(password: str)
+        switch level {
+        case 0:
+            print("bad password")
+        case 1:
+            print("the level of protection 1")
+        case 2:
+            print("the level of protection 2")
+        case 3:
+            print("the level of protection 3")
+        default:
+            break
+        }
+    }
+    
+    func checkToNumber(password: String) -> Int {
+        let allNumber = "1234567890"
+        for i in password {
+            if allNumber.contains(i) {
+                return 1
+            }
+        }
+        return 0
+    }
+    
+    func checkToUpperCase(password: String) -> Int {
+        for i in password {
+            if String(i) == String(i).uppercased() {
+                return 1
+            }
+        }
+        return 0
+    }
+    
+    func checkToSpecialChar(password: String) -> Int {
+        let specialChar = "@#$%^&*()?><:,._-"
+        for i in password {
+            if specialChar.contains(i) {
+                return 1
+            }
+        }
+        return 0
+    }
+    
     // homework 7
     func sortArray(array: [Int]) -> [Int] {
         var arrayForSorted = array
@@ -97,7 +163,17 @@ class ViewController: UIViewController {
     
     //homework 8
     func translite(str: String) -> String {
-        let alphabet = ["a": "a", "b": "б", "c": "ц", "d": "д"]
+        let alphabet = ["а": "a", "б": "b", "в": "v",
+                        "г": "g", "д": "d", "е": "e",
+                        "ё": "yo", "ж": "g", "з": "z",
+                        "и": "i", "й": "y", "к": "k",
+                        "л": "l", "м": "m", "н": "n",
+                        "о": "o", "п": "p", "р": "r",
+                        "с": "s", "т": "t", "у": "u",
+                        "ф": "f", "х": "h", "ц": "c",
+                        "ч": "ch", "ш": "sh", "щ": "ch'",
+                        "ы": "i", "э": "e", "ю": "yu",
+                        "я": "ya",]
         var transliteString = String.init()
         for i in str {
             if String(i) == String(i).lowercased() {
@@ -119,7 +195,5 @@ class ViewController: UIViewController {
         })
         print("\(filteredWords)")
     }
-    
-    
 }
 
